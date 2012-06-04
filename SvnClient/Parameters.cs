a05f1a5a3296f06cc6509af159de6e14c1a097f1
Adam Coyne <github@mail2.coyne.nu>
2012-06-04 08:50:14 -0500

    Add my customizations: Add checkoutupdate function Make SharpSvn reference less specific, and private Use Options.cs for option parsing Trust unsigned server certs Print additional commit status

---
+++ SvnClient/Parameters.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using NDesk.Options;
+
+namespace SvnClient
+{
+    public enum Command
+    {
+        UnknownDefaultThingy,
+        CompleteSync,
+        CheckoutUpdate,
+    }
+
+    public class Parameters
+    {
+        public Command Command { get; private set; } // completesync, checkoutupdate
+        public string Message { get; private set; } // commit message
+        public bool UpdateBeforeCompleteSync { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Path { get; private set; }
+        public string Url { get; private set; }
+        public bool Revert { get; private set; }
+        public bool Cleanup { get; private set; }
+        public bool DeleteUnversioned { get; private set; }
+        public bool TrustServerCert { get; private set; }
+        public bool Mkdir { get; private set; }
+
+        public static bool TryParse(IList<string> args, out Parameters parameters)
+        {
+            parameters = null;
+
+            var p = new Parameters
+                        {
+                            Message = "Committed by SvnClient",
+                        };
+
+            var optionSet = new OptionSet
+                        {
+                            {"username=", "SVN username", v => p.Username = v},
+                            {"password=", "SVN password", v => p.Password = v},
+                            {"k|trust-server-cert", "Trust unsigned/expired server certificates",v => p.TrustServerCert = (v != null)},
+                            {"m|message=", "Commit message for CompleteSync commit or CheckoutUpdate remote directory add", v => p.Message = v},
+                            {"u|updatebeforesync", "SVN update first (CompleteSync only)", v => p.UpdateBeforeCompleteSync = (v != null)},
+                            {"revert", "Revert changes first (CheckoutUpdate only)", v => p.Revert = (v != null)},
+                            {"cleanup", "CleanUp working copy first (CheckoutUpdate only)",v => p.Cleanup = (v != null)},
+                            {"deleteunversioned", "Delete unversioned files from working copy first (CheckoutUpdate only)",v => p.DeleteUnversioned = (v != null)},
+                            {"cleanworkingcopy", "Same as --revert --cleanup --deleteunversioned (CheckoutUpdate only)", v => p.Revert = p.Cleanup = p.DeleteUnversioned = (v != null)},
+                            {"mkdir", "Create the URL if it doesn't exist (CheckoutUpdate only)", v => p.Mkdir = (v != null)},
+                        };
+            var extraArgs = optionSet.Parse(args);
+            
+            if(extraArgs.Count == 0)
+            {
+                Console.Error.WriteLine("No command specified.");
+                ShowUsage(optionSet);
+                return false;
+            }
+
+            Command command;
+            if(EnumTryParse(extraArgs[0], true, out command))
+            {
+                p.Command = command;
+            }
+            else
+            {
+                Console.Error.WriteLine("Unknown command: " + extraArgs[0]);
+                ShowUsage(optionSet);
+                return false;
+            }
+
+            switch(p.Command)
+            {
+                case Command.CompleteSync:
+                    if(extraArgs.Count < 2)
+                    {
+                        Console.Error.WriteLine("path is required");
+                        ShowUsage(optionSet);
+                        return false;
+                    }
+                    p.Path = extraArgs[1];
+                    break;
+                case Command.CheckoutUpdate:
+                    if(extraArgs.Count < 3)
+                    {
+                        Console.Error.WriteLine("URL and path are required");
+                        ShowUsage(optionSet);
+                        return false;
+                    }
+                    p.Url = extraArgs[1];
+                    p.Path = extraArgs[2];
+                    break;
+            }
+
+            parameters = p;
+            return true;
+        }
+
+        private static void ShowUsage(OptionSet optionSet)
+        {
+            var commands = new List<string>(Enum.GetNames(typeof (Command)));
+            commands.RemoveAt(0);
+
+            var exeName = AppDomain.CurrentDomain.FriendlyName;
+            Console.Error.WriteLine(string.Format("{0} {{ {1} }}", exeName, string.Join(" | ", commands.ToArray())));
+            Console.Error.WriteLine(string.Format("{0} {1} PATH [options]", exeName, Command.CompleteSync));
+            Console.Error.WriteLine(string.Format("{0} {1} URL PATH [options]", exeName, Command.CheckoutUpdate));
+            optionSet.WriteOptionDescriptions(Console.Error);
+        }
+
+        private static bool EnumTryParse<TEnum>(string value, bool ignoreCase, out TEnum result)
+        {
+            if(Enum.IsDefined(typeof(TEnum), value))
+            {
+                result = (TEnum) Enum.Parse(typeof (TEnum), value);
+                return true;
+            }
+
+            if(ignoreCase)
+            {
+                foreach(var enumValue in Enum.GetNames(typeof(TEnum)))
+                {
+                    if(string.Equals(enumValue, value, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        result = (TEnum) Enum.Parse(typeof (TEnum), enumValue);
+                        return true;
+                    }
+                }
+            }
+
+            result = default(TEnum);
+            return false;
+        }
+    }
+}
--- SvnClient/Program.cs
+++ SvnClient/Program.cs
@@ -1,6 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.IO;
+using System.Net;
 using SharpSvn;
 using System.Collections.ObjectModel;
 
@@ -10,82 +10,176 @@ namespace SvnClient
     {
         static void Main(string[] args)
         {
-            using (SharpSvn.SvnClient client = new SharpSvn.SvnClient())
+            // This doesn't affect SVN server certs
+            //ServicePointManager.ServerCertificateValidationCallback += delegate { Console.WriteLine("ServerCertificateValidationCallback!"); return true; };
+
+            Parameters parameters;
+            if (!Parameters.TryParse(args, out parameters))
             {
-                // No args, nothing to do except show help.
-                if (args.Length == 0)
+                return;
+            }
+
+            switch (parameters.Command)
+            {
+                case Command.CompleteSync:
+                    CompleteSync(parameters);
+                    break;
+                case Command.CheckoutUpdate:
+                    CheckoutUpdate(parameters);
+                    break;
+            }
+        }
+
+        private static void CheckoutUpdate(Parameters parameters)
+        {
+            using (var client = new SharpSvn.SvnClient())
+            {
+                SetUpClient(parameters, client);
+
+                var target = SvnTarget.FromString(parameters.Path);
+                SvnInfoEventArgs svnInfoEventArgs;
+                SvnUpdateResult svnUpdateResult;
+
+                var nonExistentUrl = false;
+                EventHandler<SvnErrorEventArgs> ignoreNonexistent = (o, eventArgs) =>
+                                                                        {
+                                                                            nonExistentUrl = false;
+                                                                            //if (eventArgs.Exception.SubversionErrorCode == 170000)
+                                                                            if(eventArgs.Exception.Message.Contains("non-existent in revision"))
+                                                                            {
+                                                                                nonExistentUrl = true;
+                                                                                eventArgs.Cancel = true;
+                                                                            }
+                                                                        };
+
+                if(client.GetWorkingCopyRoot(parameters.Path) == null)
                 {
-                    Console.WriteLine("SVNCompleteSync - add/remove changed files");
-                    Console.WriteLine("Usage:");
-                    Console.WriteLine(" SvnCompleteSync [path]");
-                    Console.WriteLine("  -m        \"message\" (override default log message)");
-                    Console.WriteLine("  -u        update directory before processing add/remove");
-                    Console.WriteLine("  -user     username (optional)");
-                    Console.WriteLine("  -password password (optional)");
+                    client.SvnError += ignoreNonexistent;
+                    var getInfoSucceeded = client.GetInfo(SvnUriTarget.FromString(parameters.Url), out svnInfoEventArgs);
+                    client.SvnError -= ignoreNonexistent;
+
+                    if(!getInfoSucceeded)
+                    {
+                        if (nonExistentUrl)
+                        {
+                            Console.WriteLine("SVN info reported nonexistent URL; creating remote directory.");
+                            if (!client.RemoteCreateDirectory(new Uri(parameters.Url), new SvnCreateDirectoryArgs {CreateParents = true, LogMessage = parameters.Message}))
+                            {
+                                throw new Exception("Create directory failed on " + parameters.Url);
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("SVN info failed");
+                        }
+                    }
+
+                    if (client.CheckOut(SvnUriTarget.FromString(parameters.Url), parameters.Path, out svnUpdateResult))
+                    {
+                        Console.WriteLine("Checked out r" + svnUpdateResult.Revision);
+                        return;
+                    }
+
+                    throw new Exception("SVN checkout failed");
+                }
+
+                if(!client.GetInfo(target, out svnInfoEventArgs))
+                {
+                    throw new Exception("SVN info failed");
+                }
+
+                if(!UrlsMatch(svnInfoEventArgs.Uri.ToString(), parameters.Url))
+                {
+                    throw new Exception(string.Format("A different URL is already checked out ({0} != {1})", svnInfoEventArgs.Uri, parameters.Url));
+                }
+                
+                if(parameters.Cleanup)
+                {
+                    client.CleanUp(parameters.Path);
+                }
+
+                if(parameters.Revert)
+                {
+                    client.Revert(parameters.Path);
+                }
+
+                if (parameters.DeleteUnversioned)
+                {
+                    Collection<SvnStatusEventArgs> changedFiles;
+                    client.GetStatus(parameters.Path, out changedFiles);
+                    foreach (var changedFile in changedFiles)
+                    {
+                        if(changedFile.LocalContentStatus == SvnStatus.NotVersioned)
+                        {
+                            File.Delete(changedFile.FullPath);
+                        }
+                    }
+                }
+
+                if(client.Update(parameters.Path, out svnUpdateResult))
+                {
+                    Console.WriteLine("Updated to r" + svnUpdateResult.Revision);
                     return;
                 }
 
-                // first Argument : Path to folder that gets commited
-                //-m Logmessage
-                string path = args[0];
-                string logMessage = "Added by SvnCompleteSync";
-                bool isUpdate = false;
-                string sUser = string.Empty;
-                string sPassword = string.Empty;
+                throw new Exception("SVN update failed");
+            }
+        }
 
-                for (int i = 0; i <= args.Length - 1; i++)
+        private static bool UrlsMatch(string url1, string url2)
+        {
+            if (url1 == url2) return true;
+
+            // Ignore trailing slash
+            return url1.TrimEnd('/') == url2.TrimEnd('/');
+        }
+
+        // TODO: make trusting not the default
+        private static void TrustUnsignedCertificates(SharpSvn.SvnClient client)
+        {
+            client.Authentication.SslServerTrustHandlers += (sender, e) =>
+                                                                {
+                                                                    Console.WriteLine("Certificate errors: " + e.Failures);
+                                                                    e.AcceptedFailures = e.Failures;
+                                                                    e.Save = true; // Save it for future use?
+
+                                                                };
+            //client.Authentication.SslServerTrustHandlers += Authentication_SslServerTrustHandlers;
+        }
+
+        public static void CompleteSync(Parameters parameters)
+        {
+            using (var client = new SharpSvn.SvnClient())
+            {
+                SetUpClient(parameters, client);
+
+                if (parameters.UpdateBeforeCompleteSync)
                 {
-                    if (args[i] == "-m")
-                    {
-                        logMessage = args[i + 1];
-                    }
-                    if (args[i] == "-u")
-                    {
-                        isUpdate = true;
-                    }
-                    if (string.Compare(args[i], "-user", true) == 0)
-                    {
-                        sUser = args[i + 1];
-                        i++; // advance past username
-                    }
-                    if (string.Compare(args[i], "-password", true) == 0)
-                    {
-                        sPassword = args[i + 1];
-                        i++; // advance past password
-                    }
+                    client.Update(parameters.Path);
                 }
 
-                // optionally, set authentication
-                if ((sUser != string.Empty) && (sPassword != string.Empty))
+                Collection<SvnStatusEventArgs> changedFiles;
+                client.GetStatus(parameters.Path, out changedFiles);
+                if(changedFiles.Count == 0)
                 {
-                    client.Authentication.Clear(); // prevents checking cached credentials
-                    client.Authentication.DefaultCredentials = new System.Net.NetworkCredential(sUser, sPassword);
+                    Console.WriteLine("No changes to commit.");
                 }
-
-                if (isUpdate)
-                {
-                    client.Update(path);
-                }
-
-                Collection<SvnStatusEventArgs> changedFiles = new Collection<SvnStatusEventArgs>();
-                client.GetStatus(path, out changedFiles);
 
                 //delete files from subversion that are not in filesystem
                 //add files to subversion that are new in filesystem
                 //modified files are automatically included as part of the commit
 
                 //TODO: check remoteStatus
-                foreach (SvnStatusEventArgs changedFile in changedFiles)
+                foreach (var changedFile in changedFiles)
                 {
                     if (changedFile.LocalContentStatus == SvnStatus.Missing)
                     {
                         // SVN thinks file is missing but it still exists hence
                         // a change in the case of the filename.
-                        if (System.IO.File.Exists(changedFile.Path))
+                        if (File.Exists(changedFile.Path))
                         {
-                            SvnDeleteArgs changed_args = new SvnDeleteArgs();
-                            changed_args.KeepLocal = true;
-                            client.Delete(changedFile.Path, changed_args);
+                            var changedArgs = new SvnDeleteArgs {KeepLocal = true};
+                            client.Delete(changedFile.Path, changedArgs);
                         }
                         else
                             client.Delete(changedFile.Path);
@@ -96,11 +190,29 @@ namespace SvnClient
                     }
                 }
 
-                SvnCommitArgs ca = new SvnCommitArgs();
-                ca.LogMessage = logMessage;
+                var ca = new SvnCommitArgs {LogMessage = parameters.Message};
+                SvnCommitResult result;
+                client.Commit(parameters.Path, ca, out result);
+                if(!string.IsNullOrEmpty(result.PostCommitError))
+                {
+                    Console.WriteLine("Post-commit hook error: " + result.PostCommitError);
+                    return;
+                }
+                Console.WriteLine("Committed r" + result.Revision);
+            }
+        }
 
-                client.Commit(path, ca);
+        private static void SetUpClient(Parameters parameters, SharpSvn.SvnClient client)
+        {
+            if (!string.IsNullOrEmpty(parameters.Username) && !string.IsNullOrEmpty(parameters.Password))
+            {
+                client.Authentication.Clear(); // prevents checking cached credentials
+                client.Authentication.DefaultCredentials = new NetworkCredential(parameters.Username, parameters.Password);
+            }
 
+            if(parameters.TrustServerCert)
+            {
+                TrustUnsignedCertificates(client);
             }
         }
     }
